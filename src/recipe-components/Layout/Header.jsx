110bbfbb8e5d1b7868512643b5e362c7e01041e6
NguyenTuanAnh <nguyenanh1812zxc@gmail.com>
2022-09-05 21:19:55 +0700

    add router

---
+++ src/recipe-components/Layout/Header.jsx
@@ -0,0 +1,22 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Header() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Home</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/home">Recipes</Nav.Link>
+            <Nav.Link href="list-item">Shopping List</Nav.Link>
+            <Nav.Link href="#">List Bill</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default Header;
