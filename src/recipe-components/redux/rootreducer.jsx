e6d70e026152f70089a771575ad7c6b8f81e2c17
maihuy243 <maihuy243@gmail.com>
2022-09-05 21:59:15 +0700

    new future

--- src/recipe-components/redux/rootreducer.jsx
+++ src/recipe-components/redux/rootreducer.jsx
@@ -6,6 +6,20 @@ const initState = [
     img: '',
     spice: { muoi: 1, mam: 2 },
   },
+  {
+    id: 2,
+    name: 'Bánh Mì',
+    amount: 2,
+    img: '',
+    spice: {},
+  },
+  {
+    id: 3,
+    name: 'Bánh Bao',
+    amount: 1,
+    img: '',
+    spice: { muoi: 2 },
+  },
 ];
 
 export const rootReducer = (state = initState, action) => {
