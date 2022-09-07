a986ce2371f8b5d8f5c327b9df12747045eb8dcc
NguyenTuanAnh <nguyenanh1812zxc@gmail.com>
2022-09-08 00:58:14 +0700

    restructure the app

--- src/recipe-components/Home.jsx
+++ src/recipe-components/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import ListItem from './ListItem'
+import Recipes from './Recipes'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='row m-0'>
+      <div className="col-md-6"><Recipes /></div>
+      <div className="col-md-6"><ListItem /></div>
+    </div>
   )
 }
