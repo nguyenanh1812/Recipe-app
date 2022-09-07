b23e24a5776382d6b1a5759da172fe4b7fc48fa5
NguyenTuanAnh <nguyenanh1812zxc@gmail.com>
2022-09-08 00:43:30 +0700

    add redux core & config full action, selector & add recipe to Recipes + add Shopping order to ShoppingList

--- src/recipe-components/Recipes.jsx
+++ src/recipe-components/Recipes.jsx
@@ -1,63 +1,112 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { v4 as uuidv4 } from "uuid";
+import { useDispatch, useSelector } from "react-redux";
+import { addRecipe, updateRecipe, removeRecipe } from "../redux/actions";
+import { recipeListSelector } from "../redux/selectors";
 
 export default function Recipes() {
   const { register, handleSubmit } = useForm();
-  const [data, setData] = useState("");
+  const [data, setData] = useState({
+    id: "",
+    name: "",
+    description: "",
+    imgURL: "",
+  });
   const [displayForm, setDisplayForm] = useState(false);
   const [displayInput, setDisplayInput] = useState(false);
+  const dispatch = useDispatch();
+  const recipeList = useSelector(recipeListSelector);
+  // console.log(recipeList);
+  // const recipe = JSON.parse(data)
 
   const handelDisplay = () => {
-    setDisplayForm(!displayForm)
-  }
+    setDisplayForm(!displayForm);
+  };
   const handelInput = () => {
-    setDisplayInput(!displayInput)
-  }
+    setDisplayInput(!displayInput);
+  };
+
+  const handleAddButtonClick = (a) => {
+    const recipe = { id: uuidv4(), ...a };
+    dispatch(addRecipe(recipe));
+    // console.log('state id recipes',recipe);
+  };
+
+  useEffect(() => {
+    console.log("state recipes", data);
+  }, [data]);
+
   return (
     <div className="container mt-4">
-      <Button variant="success" onClick={handelDisplay}>New Recipe</Button>
+      <Button variant="success" onClick={handelDisplay}>
+        New Recipe
+      </Button>
       <h1 className="text-center">Please select a Recipe!</h1>
       <hr />
       <div className="row">
         <div className="col-md-6">
-          <div className="d-flex justify-content-between align-items-center list-item flex-wrap border rounded p-2 bg-white text-black">
-            <div className=" content">
-              <h3>Hamburger</h3>
-              <p>Hamburger</p>
+          {recipeList.map((recipe) => (
+            <div
+              key={recipe.id}
+              className="d-flex justify-content-between align-items-center list-item flex-wrap border rounded p-2 bg-white text-black mb-4"
+              style={{ minHeight: "100px", cursor: "pointer" }}
+            >
+              <div className=" content">
+                <h3>{recipe.name}</h3>
+                <p>{recipe.description}</p>
+              </div>
+              <img
+                src={recipe.imgURL}
+                alt={`img-recipe-${recipe.name}`}
+                width="100px"
+                style={{ maxWidth: "100px" }}
+              />
             </div>
-            <img
-              src="https://www.thatlangon.com/wp-content/uploads/2021/01/hamburger-thatlangon-500x500.jpg"
-              alt=""
-              width="100px"
-            />
-          </div>
+          ))}
         </div>
         <div className="col-md-6">
-          {displayForm &&
+          {displayForm && (
             <>
-
               <form
-                onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}
+                onSubmit={handleSubmit((data) => {
+                  setData(data);
+                  handleAddButtonClick(data);
+                })}
               >
-              <Button variant="success" type="submit">Save</Button>
-              <Button variant="danger" type="reset">Cancel</Button>
+                <div className="mb-2">
+                  <Button
+                    variant="success"
+                    type="submit"
+                    className="me-2"
+                    // onClick={handleAddButtonClick(data)}
+                  >
+                    Save
+                  </Button>
+                  <Button variant="danger" type="reset">
+                    Cancel
+                  </Button>
+                </div>
                 <input {...register("name")} placeholder="Name" />
-                <input {...register("imageURL")} placeholder="Image URL" />
-                <textarea {...register("description")} placeholder="Description" />
-                <p>{data}</p>
-
+                <input {...register("imgURL")} placeholder="Image URL" />
+                <textarea
+                  {...register("description")}
+                  placeholder="Description"
+                />
               </form>
-              {displayInput &&
-                <div >
+              {displayInput && (
+                <div>
                   <input type="text" />
                   <input type="text" />
                 </div>
-              }
-              <button className="submit-add" onClick={handelInput}> Add ingredient</button>
+              )}
+              <button className="submit-add" onClick={handelInput}>
+                Add ingredient
+              </button>
             </>
-          }
+          )}
         </div>
       </div>
     </div>
